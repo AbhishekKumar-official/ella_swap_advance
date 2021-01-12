14087b8258b9eefcec70125846249a0f4742a5aa
Gurpreet <gurpreetkaur.devgun@yahoo.com>
2021-01-12 09:32:04 -0800

    no message

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {lightTheme,darkTheme,GlobalStyle} from "./themes"
 import Drawer from "../src/Components/Sidebar/Drawer"
 import Configuration from "./pages/Configuration"
 import UserAccount from "./pages/UserAccount"
+import DextBoard from "./pages/DextBoard"
 import "./App.css"
 
 const StyledDiv = styled.div`
@@ -24,6 +25,7 @@ const App = () => {
         <StyledDiv>
           <Drawer themeToggler={handleThemeToggle}/>
           <Switch>
+            <Route exact path="/dextboard" component={DextBoard}/>
             <Route exact path="/user-account" component={UserAccount}/>
             <Route exact path="/configuration" component={Configuration}/>
           </Switch>
