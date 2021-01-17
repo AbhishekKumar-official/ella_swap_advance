30df21ea2ffa5600e5729dc16b2d83482f870988
Gurpreet <gurpreetkaur.devgun@yahoo.com>
2021-01-17 05:13:16 -0800

    no message

--- src/pages/UserAccount.jsx
+++ src/pages/UserAccount.jsx
@@ -1,8 +1,30 @@
 import React, { Component } from 'react'
 import banner from "../images/banner.gif"
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import walletConnect from "../images/wallet_connect.svg"
+import walletLink from "../images/wallet_link.svg"
+import torus from "../images/torus.svg"
+import fortmatic from "../images/fortmatic.svg"
+import authereum from "../images/authereum.svg"
+import mewWallet from "../images/mew_wallet.png"
 import "./UserAccount.scss"
 class UserAccount extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            connect:false,
+        }
+    }
+    handleCloseConnect = () => {
+        this.setState({
+            connect: false
+        })
+    }
     render() {
+        const {connect} = this.state;
         return (
             <div className="main-useraccount">
                 <div className="banner text-center d-sm-none d-md-none d-lg-block d-none">
@@ -24,7 +46,7 @@ class UserAccount extends Component {
                             <div className="card-body text-center">
                                 <h5 className="auth-form__title text-center mb-4"> Connect your Wallet </h5>
                                 <p className="text-center">Connect your wallet with Dextools.</p>
-                                <button type="button" className="btn btn-info d-table mx-auto"> Connect </button>
+                                <button type="button" className="btn btn-info d-table mx-auto" onClick={()=>{this.setState({connect:true})}}> Connect </button>
                             </div>
                         </div>
                         <div className="card mb-5">
@@ -43,6 +65,70 @@ class UserAccount extends Component {
                         </div>
                     </div>
                 </div>
+                <Dialog
+            open={connect}
+            onClose={this.handleCloseConnect}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+            className="modal-connect"
+          >
+            <DialogContent>
+                <div class="connect-wrapper">
+                    <div class="connect-container">
+                        <div class="connect-icon">
+                        <img src={walletConnect} />
+                        </div>
+                        <div class="connect-name">WalletConnect</div>
+                        <div class="connect-description">Scan with WalletConnect to connect</div>
+                    </div>
+                </div>
+                <div class="connect-wrapper">
+                    <div class="connect-container">
+                        <div class="connect-icon">
+                        <img src={walletLink} />
+                        </div>
+                        <div class="connect-name">WalletLink</div>
+                        <div class="connect-description">Scan with WalletLink to connect</div>
+                    </div>
+                </div>
+                <div class="connect-wrapper">
+                    <div class="connect-container">
+                        <div class="connect-icon">
+                        <img src={torus} />
+                        </div>
+                        <div class="connect-name">Torus</div>
+                        <div class="connect-description">Connect with your Torus account</div>
+                    </div>
+                </div>
+                <div class="connect-wrapper">
+                    <div class="connect-container">
+                        <div class="connect-icon">
+                        <img src={fortmatic} />
+                        </div>
+                        <div class="connect-name">Fortmatic</div>
+                        <div class="connect-description">Connect with your Fortmatic account</div>
+                    </div>
+                </div>
+                <div class="connect-wrapper">
+                    <div class="connect-container">
+                        <div class="connect-icon">
+                        <img src={authereum} />
+                        </div>
+                        <div class="connect-name">Authereum</div>
+                        <div class="connect-description">Connect with your Authereum account</div>
+                    </div>
+                </div>
+                <div class="connect-wrapper">
+                    <div class="connect-container">
+                        <div class="connect-icon">
+                        <img src={mewWallet} />
+                        </div>
+                        <div class="connect-name">MEW wallet</div>
+                        <div class="connect-description">Scan with MEW wallet to connect</div>
+                    </div>
+                </div>
+            </DialogContent>
+          </Dialog>
             </div>
         )
     }
